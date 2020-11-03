1a1d93f7ab83c1c3019d27a8f7dd5b067c077989
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-11-02 18:27:10 -0800

    add css folder and file, also will add all required homework code for project file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-//business logic for address book...//
-
+// Business Logic for AddressBook ---------
 function AddressBook() {
-  this.contacts =[];
+  this.contacts = [];
   this.currentId = 0;
 }
 
@@ -27,8 +26,8 @@ AddressBook.prototype.findContact = function(id) {
 }
 
 AddressBook.prototype.deleteContact = function(id) {
-  for (let i=0; i<this.contacts.length; i++) {
-    if (this.contacts[i]); {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
       if (this.contacts[i].id == id) {
         delete this.contacts[i];
         return true;
@@ -38,7 +37,7 @@ AddressBook.prototype.deleteContact = function(id) {
   return false;
 }
 
-//business logic for contacts...//
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -46,7 +45,57 @@ function Contact(firstName, lastName, phoneNumber) {
 }
 
 Contact.prototype.fullName = function() {
-  return this.firstName + "" + this.lastName;
+  return this.firstName + " " + this.lastName;
 }
 
+// User Interface Logic ---------
+let addressBook = new AddressBook();
 
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function() {
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
+
+$(document).ready(function() {
+  attachContactListeners();
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  })
+})
